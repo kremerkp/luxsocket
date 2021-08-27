d11d996161205c20a70cfe7c9326c91687553b9b
Kai Kremer <kremerkp@gmail.com>
2021-08-27 17:35:24 +0200

    test

--- src/main/java/lu/lux/socket/AppointmentController.java
+++ src/main/java/lu/lux/socket/AppointmentController.java
@@ -8,24 +8,24 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class AppointmentController {
-	
+
 	@Autowired
 	AppointmentRepository appointmentRepository;
-	
+
 	//private final SimpMessagingTemplate simpMessagingTemplate = null;
-	
+
 	@Autowired
 	private SimpMessagingTemplate simpMessagingTemplate;
-    
+
     @MessageMapping("/refreshAppointments")
     @SendTo("/luxablApp/appointments")
     public SimpleRoutingAppointment greet3(Long appointmentId) throws Exception {
     	Thread.sleep(10); // simulated delay
-		SimpleRoutingAppointment simp =  appointmentRepository.findSimpleRoutingAppointmentsById(appointmentId); 
+		SimpleRoutingAppointment simp =  appointmentRepository.findSimpleRoutingAppointmentsById(appointmentId);
         return simp;
-        
+
     }
-    
+
 
 
 }
